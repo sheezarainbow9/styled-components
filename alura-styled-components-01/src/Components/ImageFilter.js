683111bf3cdc40fdf91613c8720c765fce17ab38
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-10 12:22:06 -0300

    mudar tema

--- alura-styled-components-01/src/Components/ImageFilter.js
+++ alura-styled-components-01/src/Components/ImageFilter.js
@@ -4,15 +4,15 @@ import outros from "../assets/images/outros.svg";
 import saude from "../assets/images/saude.svg";
 import transporte from "../assets/images/transporte.svg";
 import utilidades from "../assets/images/utilidades.svg";
-import { Icone } from "../Components/UI";
+import { IconeTema } from "../Components/UI";
 
 export default (type) => {
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-    Outros: <Icone src={outros} alt="Outros" />,
-    Saude: <Icone src={saude} alt="Saúde" />,
-    Transporte: <Icone src={transporte} alt="Transporte" />,
-    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Outros: <IconeTema src={outros} alt="Outros" />,
+    Saude: <IconeTema src={saude} alt="Saúde" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
   };
 
   return Images[type] || Images.default;
